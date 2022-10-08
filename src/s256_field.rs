4ba5ad8eb45ef8b319e07734bb7f43ce6092e100
shinbunbun <takamuneyuto@gmail.com>
2022-10-08 15:44:49 +0900

    Revert "fix s256_field"
    This reverts commit 23b4199550a5ada194842bfbed953eae6c36e6bf.

--- src/s256_field.rs
+++ src/s256_field.rs
@@ -7,15 +7,15 @@ use crate::field_element::FieldElement;
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct S256Field {
     field: FieldElement<Integer>,
-    prime: Integer,
 }
 
 impl S256Field {
     pub fn new(num: Integer) -> Self {
-        let p = Integer::from(2).pow(256) - Integer::from(2).pow(32) - Integer::from(977);
         Self {
-            field: FieldElement::new(num, p.clone()),
-            prime: p,
+            field: FieldElement::new(
+                num,
+                Integer::from(2).pow(256) - Integer::from(2).pow(32) - 977,
+            ),
         }
     }
 }
