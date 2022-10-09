25839209eb6e2d408c9083cafce9c9103c02c9f2
shinbunbun <takamuneyuto@gmail.com>
2022-10-09 15:03:51 +0900

    [WIP] 署名の実装

--- src/private_key.rs
+++ src/private_key.rs
@@ -1,6 +1,6 @@
 use rug::Integer;
 
-use crate::{field_element::FieldElement, point::Point, secp256k1::get_g};
+use crate::{field_element::FieldElement, point::Point, random, secp256k1};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PrivateKey {
@@ -8,15 +8,20 @@ pub struct PrivateKey {
     pub point: Point<FieldElement<Integer>, Integer>,
 }
 
-impl PrivateKey {
+/* impl PrivateKey {
     pub fn new(secret: Integer) -> Self {
         Self {
             secret: secret.clone(),
-            point: get_g() * secret,
+            point: secp256k1::get_g() * secret,
         }
     }
 
-    /* pub fn sign(&self, z: Integer) -> Signature {
-        let k =
-    } */
-}
+    pub fn sign(&self, z: Integer) -> Signature {
+        let n = secp256k1::get_n();
+        let g = secp256k1::get_g();
+        let k = random::random();
+        let r = (g.clone() * k).x.unwrap();
+        let k_inv = k.pow_mod(n.clone() - 2, &n).unwrap();
+        let s = (z + r.clone() * self.secret.clone()) * k_inv % &n;
+    }
+} */
